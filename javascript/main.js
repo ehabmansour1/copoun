0515505a6a27aeca582a50e2f1146148462030ed
Ehab Mansour <81818344+ehabmansour1@users.noreply.github.com>
2023-03-24 22:15:05 +0200

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -22,14 +22,6 @@ paragraphs.forEach((paragraph) => {
 
     // Log a message to the console
     console.log("Copied to clipboard:", paragraph.innerHTML);
-    Swal.fire({
-      title: "تم نسخ الكود بنجاح",
-      text: " اضغط موافق واستمتع بالخصم",
-      icon: "success",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        window.location.href = "https://ar.thegivingmovement.com/";
-      }
-    });
+    window.location.href = "https://ar.thegivingmovement.com/";
   });
 });
